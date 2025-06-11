6369699f33376b4fc3ff1d099cfe7412ea018bca
quentin smith <quesmith@pdx.edu>
2025-06-10 18:59:08 -0700

    project finished

---
+++ Readme.md
@@ -0,0 +1,8 @@
+### Final Project Website:
+
+This is a simple webpage displaying some of my history with CS in school. It displays information using various elements using HTML, css, bootstrap, and javascript.
+
+### Sources used:
+
+- [Bootstrap docs](https://getbootstrap.com/docs/5.0/getting-started/introduction/)
+- [Javascript docs](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
--- final.js
+++ final.js
@@ -1,1 +1,32 @@
 // add active link function for navbar
+const links = document.querySelectorAll(".nav-link");
+
+if (links.length) {
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      links.forEach((link) => {
+        link.classList.remove("active");
+      });
+      e.preventDefault();
+      link.classList.add("active");
+    });
+  });
+}
+
+const form = document.getElementById("form1");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(form);
+
+  const name = formData.get("name");
+  const email = formData.get("email");
+  const pronouns = formData.get("pronouns");
+  const message = formData.get("message");
+  console.log("Form submitted with values:");
+  console.log("Name:", name);
+  console.log("Email:", email);
+  console.log("Pronouns:", pronouns);
+  console.log("Message:", message);
+});
