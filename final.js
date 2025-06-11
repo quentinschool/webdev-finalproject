74d8e509a907fe6e667e2ac72637b022ae07824b
quentin smith <quesmith@pdx.edu>
2025-06-10 19:57:30 -0700

    updated

--- final.js
+++ final.js
@@ -1,10 +1,7 @@
-// add active link function for navbar
 var btnContainer = document.getElementById("navbarNavAltMarkup");
 
-// Get all buttons with class="btn" inside the container
 var btns = btnContainer.getElementsByClassName("nav-link");
 
-// Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function () {
     var current = document.getElementsByClassName("active");
